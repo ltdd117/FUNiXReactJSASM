c7d26e4bb5e2ba686ab158bd4359274480973f7f
Dat La <latandat117@gmail.com>
2022-02-14 13:26:09 +0900

    Presentational and Container Components

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -12,13 +12,11 @@ class Main extends Component {
         this.state = {
             dishes: DISHES,
             selectedDish: null,
-            comments: null
         };
     }
 
     onDishSelect(dishId, dishComments) {
         this.setState({ selectedDish: dishId });
-        this.setState({ comments: dishComments });
     }
 
     render() {
@@ -30,9 +28,8 @@ class Main extends Component {
                 <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
             </Navbar>
-            <Menu dishes={this.state.dishes} onClick={(dishId, dishComments) => this.onDishSelect(dishId, dishComments)} />
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
-                />
+            <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         </div>
         );
     }
