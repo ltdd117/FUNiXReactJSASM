1c2f9c6abda87a51fd6b900887f3e58f4465f9e2
Dat La <latandat117@gmail.com>
2022-02-17 20:15:01 +0900

    edit ex2

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { useState } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function Contact(props) {
+
+    const [state, setState] = useState({ firstname: "", lastname: "", email: "", telnum: "", agree: false, contactType: "Tel.", message: "" });
+
+    const handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        setState({ ...state, [name]: value });
+    }
+
+    const handleSubmit = (event) => {
+        console.log('Current State is: ' + JSON.stringify(state));
+        alert('Current State is: ' + JSON.stringify(state));
+        event.preventDefault();
+    }
+
     return(
         <div className="container">
             <div className="row">
@@ -15,6 +32,89 @@ function Contact(props) {
                     <h3>Contact Us</h3>
                     <hr />
             </div>   
+            <div className='row row-content'>
+                <div className='col-12 col-md-9'>
+                    <h3><strong>Send us your Feedback</strong></h3>
+                </div>
+                <div className='col-12 col-md-9'>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup row className='space'>
+                            <Label htmlFor='firstname' md={2}>First Name</Label>
+                            <Col md={10}>
+                                <Input type='text' id='firstname' name='firstname'
+                                    placeholder='First Name'
+                                    value={state.firstname}
+                                    onChange={handleInputChange} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row className='space'>
+                            <Label htmlFor="lastname" md={2}>Last Name</Label>
+                            <Col md={10}>
+                                <Input type="text" id="lastname" name="lastname"
+                                    placeholder="Last Name"
+                                    value={state.lastname}
+                                    onChange={handleInputChange} />
+                            </Col>                        
+                        </FormGroup>
+                        <FormGroup row className='space'>
+                            <Label htmlFor='telnum' md={2}>Contact Tel.</Label>
+                            <Col md={10}>
+                                <Input type='tel' id='telnum' name='telnum'
+                                    placeholder='Tel. number'
+                                    value={state.telnum}
+                                    onChange={handleInputChange} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row className='space'>
+                            <Label htmlFor='email' md={2}>Email</Label>
+                            <Col md={10}>
+                                <Input type='email' id='email' name='email'
+                                    placeholder='Email'
+                                    value={state.email}
+                                    onChange={handleInputChange} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row className='space-center space'>
+                            <Col md={{size: 6, offset: 2}}>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="checkbox"
+                                            name="agree"
+                                            checked={state.agree}
+                                            onChange={handleInputChange} /> {' '}
+                                        <strong>May we contact you?</strong>
+                                    </Label>
+                                </FormGroup>
+                            </Col>
+                            <Col md={{size: 3, offset: 1}}>
+                                <Input type="select" name="contactType"
+                                    value={state.contactType}
+                                    onChange={handleInputChange}>
+                                    <option>Tel.</option>
+                                    <option>Email</option>
+                                </Input>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row className='space'>
+                            <Label htmlFor="message" md={2}>Your Feedback</Label>
+                            <Col md={10}>
+                                <Input type="textarea" id="message" name="message"
+                                    rows="12"
+                                    value={state.message}
+                                    onChange={handleInputChange}></Input>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row className='space'>
+                            <Label md={2}></Label>
+                            <Col md={{size: 10, offset: 2}}>
+                                <Button type="submit" color="primary">
+                                    Send Feedback
+                                </Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </div>
+            </div>
             <div className="row row-content">
                 <div className="col-12">
                 <h3>Location Information</h3>
@@ -33,15 +133,13 @@ function Contact(props) {
                 <div className="col-12 col-sm-6 offset-sm-1">
                     <h5>Map of our Location</h5>
                 </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
+                <div className="col-12 col-sm-11 offset-sm-1 ">
                     <div className="btn-group" role="group">
                         <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
                         <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
                         <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                     </div>
                 </div>
-                
-                             
             </div>
         </div>
     );
