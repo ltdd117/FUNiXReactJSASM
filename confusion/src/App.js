9e900d4a1ecb5d89abc2cea836c85a2d76385638
Dat La <latandat117@gmail.com>
2022-02-23 19:46:43 +0900

    Presentational and Container Components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponents';
 import React, { Component } from 'react';
-import { DISHES } from './shared/dishes';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );
-  }
+  
+	render() {
+		return (
+		<div >
+			<Main />
+		</div>
+		);
+	}
 }
 
 export default App;
---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle,
+    Breadcrumb, BreadcrumbItem } from 'reactstrap';
+
+function RenderDish({dish}) {
+    if (dish != null) {
+        return(
+            <div className='col-12 col-md-5 m-1'>
+                <Card key={dish.id}>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+    else
+        return(
+          	<div></div>
+        );
+}
+
+function RenderComments({comments}) {
+    //Get the comments array using props keyword
+    if (comments != null) {
+        return(
+            <div className='col-12 col-md-5 m-1'>
+                <h4>Comments</h4>
+                <ul className='list-unstyled'>
+                        {comments.comments.map((comment) => {
+                            return(
+                                <div id={comment.id}>
+                                    <li>
+                                        <p>{comment.comment}</p>
+                                        <p>{comment.rating} stars</p>
+                                        <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </li>
+                                </div>
+                            );
+                        })}
+                </ul>
+            </div>
+        );
+    }
+    else
+        return(
+            <div></div>
+        );
+}
+
+// --Render Dish--
+const DishDetail = (props) => {
+    if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)        
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.dish} />
+                </div>
+                
+            </div>
+        );
+    else
+        return(
+            <div></div>
+        );
+}
+
+export default DishDetail;
